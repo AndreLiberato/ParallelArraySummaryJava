d986f05a5dc0996955877f6b6f973cfc7423ed1f
Pedro Medeiros <plfigueiredo.27@gmail.com>
2023-10-24 15:03:18 -0300

    feat: impl. do gerenciamento de saida

---
+++ src/main/java/br/ufrn/imd/utils/Output.java
@@ -0,0 +1,20 @@
+package br.ufrn.imd.utils;
+
+import br.ufrn.imd.model.Result;
+
+public class Output {
+
+    public static void showResult(Result result){
+        System.out.println("Soma total = "+result.totalElements());
+
+        System.out.println("Soma total por grupo:");
+        result.totalByGroup().forEach((key, value) -> System.out.println("Grupo "+key+": "+value));
+
+        System.out.println("Ids menores que 5: ");
+        result.idElementsLessThanFive().forEach(System.out::println);
+
+        System.out.println("Ids maiores ou igual que 5: ");
+        result.idElementsGreaterOrEqualToFive().forEach(System.out::println);
+    }
+
+}
