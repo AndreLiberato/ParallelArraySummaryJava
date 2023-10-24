4e14b3d40eda19b83bfc9729db900e9f191ea45a
André Liberato <andre.liberato@outlook.com.br>
2023-10-23 23:34:49 -0300

    Alterando o main para o novo uso do processor (#6)

--- src/main/java/br/ufrn/imd/Main.java
+++ src/main/java/br/ufrn/imd/Main.java
@@ -1,16 +1,16 @@
 package br.ufrn.imd;
 
-import br.ufrn.imd.model.ParallelArraySummary;
+import br.ufrn.imd.utils.Processor;
 
 public class Main {
 
     public static void main(String[] args) {
         final int N = 2;
 
-        ParallelArraySummary parallelArraySummary = new ParallelArraySummary();
-        parallelArraySummary.loadElements(N);
+        Processor p = new Processor();
 
-        parallelArraySummary.getElements().forEach(element -> System.out.println(element.toString()));
+        p.loadElements(N);
 
+        p.resolve();
     }
 }
