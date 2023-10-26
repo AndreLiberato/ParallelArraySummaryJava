7cc4173cb417ebfd8a13b710d683c7a72b7acfd2
André Liberato <andre.liberato@outlook.com.br>
2023-10-25 21:15:02 -0300

    Ajustando prints (#8)

--- src/main/java/br/ufrn/imd/utils/Output.java
+++ src/main/java/br/ufrn/imd/utils/Output.java
@@ -4,17 +4,17 @@ import br.ufrn.imd.model.Result;
 
 public class Output {
 
-    public static void showResult(Result result){
-        System.out.println("Soma total = "+result.totalElements());
+    public static void showResult(Result result) {
+        System.out.println("Soma total = " + result.totalElements());
 
         System.out.println("Soma total por grupo:");
-        result.totalByGroup().forEach((key, value) -> System.out.println("Grupo "+key+": "+value));
+        result.totalByGroup().forEach((key, value) -> System.out.println("Grupo " + key + ": " + value));
 
         System.out.println("Ids menores que 5: ");
-        result.idElementsLessThanFive().forEach(System.out::println);
+        result.idElementsLessThanFive().forEach(id -> System.out.print(id + " "));
 
         System.out.println("Ids maiores ou igual que 5: ");
-        result.idElementsGreaterOrEqualToFive().forEach(System.out::println);
+        result.idElementsGreaterOrEqualToFive().forEach(id -> System.out.print(id + " "));
     }
 
 }
