208c7ae87b4ad1cd33f35f62a27e76659aab5900
André Liberato <andre.liberato@outlook.com.br>
2023-10-25 21:16:48 -0300

    Implementando concorrência (#8)

--- src/main/java/br/ufrn/imd/model/IOperationImpl.java
+++ src/main/java/br/ufrn/imd/model/IOperationImpl.java
@@ -1,42 +1,71 @@
 package br.ufrn.imd.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class IOperationImpl implements IOperation{
+public class IOperationImpl implements IOperation {
 
-    @Override
-    public double sumTotal(List<Element> elements) {
-        double total = 0;
-        for(Element element : elements){
-            total += element.getTotal();
-        }
-        return total;
+    List<Element> elements;
+    double sumTotal;
+    Map<Integer, Double> sumByGroup;
+    List<Integer> lessThanFive;
+    List<Integer> greaterThanFive;
+
+    public IOperationImpl(List<Element> elements) {
+        this.elements = elements;
+        sumTotal = 0.0;
+        lessThanFive = new ArrayList<Integer>();
+        greaterThanFive = new ArrayList<Integer>();
+        sumByGroup = new HashMap<Integer, Double>();
+    }
+
+    public List<Element> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<Element> elements) {
+        this.elements = elements;
     }
 
     @Override
-    public Map<Integer, Double> sumTotalByGroup(List<Element> elements) {
-        Map<Integer, Double> sumTotalMap = new ConcurrentHashMap<>();
-        for(Element element : elements){
+    public void sumTotal() {
+        for (Element element : elements) {
+            sumTotal += element.getTotal();
+        }
+    }
 
-            double sumTotal = Objects.isNull(sumTotalMap.get(element.getGroup())) ? 0 : sumTotalMap.get(element.getGroup());
+    @Override
+    public void sumTotalByGroup() {
+        for (Element element : elements) {
+            double sumTotal = Objects.isNull(sumByGroup.get(element.getGroup())) ? 0
+                    : sumByGroup.get(element.getGroup());
             double total = sumTotal + element.getTotal();
-            sumTotalMap.put(element.getGroup(), total);
+            sumByGroup.put(element.getGroup(), total);
         }
-        return sumTotalMap;
-    }
-
-
-    @Override
-    public List<Integer> filterIdByTotalLessThanFive(List<Element> elements) {
-        return elements.stream().filter(element -> element.getTotal() < 5).map(Element::getId).toList();
     }
 
     @Override
-    public List<Integer> filterIdByTotalGreaterThanFive(List<Element> elements) {
-        return elements.stream().filter(element -> element.getTotal() >= 5).map(Element::getId).toList();
+    public void filterIdByTotalLessThanFive() {
+        lessThanFive = elements.stream().filter(element -> element.getTotal() < 5).map(Element::getId).toList();
     }
 
+    @Override
+    public void filterIdByTotalGreaterThanFive() {
+        greaterThanFive = elements.stream().filter(element -> element.getTotal() >= 5).map(Element::getId).toList();
+    }
+
+    public Result gerPartialResult() {
+        return new Result(sumTotal, sumByGroup, lessThanFive, greaterThanFive);
+    }
+
+    @Override
+    public void run() {
+        this.sumTotal();
+        this.sumTotalByGroup();
+        this.filterIdByTotalLessThanFive();
+        this.filterIdByTotalGreaterThanFive();
+    }
 }
