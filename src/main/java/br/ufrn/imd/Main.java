861ccd5e7b12cd8aa5661b60b2d3a720a55ccddb
André Liberato <andre.liberato@outlook.com.br>
2023-10-25 21:14:27 -0300

    Pequeno ajuste para nova implementação da concorrência (#8)

--- src/main/java/br/ufrn/imd/Main.java
+++ src/main/java/br/ufrn/imd/Main.java
@@ -7,13 +7,14 @@ import br.ufrn.imd.utils.Output;
 public class Main {
 
     public static void main(String[] args) {
-        final int N = 2;
+        final int N = 7;
+        final int numThreads = 20;
 
         Executor p = new Executor();
 
         p.loadElements(N);
 
-        Result output = p.process();
+        Result output = p.process(numThreads);
 
         Output.showResult(output);
     }
