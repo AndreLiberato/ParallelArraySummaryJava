3ef50cd3b25436b537954452ef93278addabb159
pedrolucas27 <plfigueiredo.27@gmail.com>
2023-10-27 13:33:28 -0300

    refactor: sincronização na junção dos resultados parciais

--- src/main/java/br/ufrn/imd/model/Executor.java
+++ src/main/java/br/ufrn/imd/model/Executor.java
@@ -1,72 +1,50 @@
 package br.ufrn.imd.model;
 
-import br.ufrn.imd.utils.ResultProcessor;
 import br.ufrn.imd.utils.RandomGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CyclicBarrier;
 
 public class Executor {
 
-    private ParallelArraySummary parallelArraySummary;
+    private List<Element> elements;
 
     public Executor() {
-        this.parallelArraySummary = new ParallelArraySummary();
+        this.elements = new ArrayList<>();
     }
 
     public void loadElements(int N) {
         final int size = (int) Math.pow(10, N);
         for (int index = 0; index < size; index++) {
-            this.parallelArraySummary
-                    .addElement(new Element(index, RandomGenerator.generateValue(), RandomGenerator.generateGroup()));
+            this.elements.add(new Element(index, RandomGenerator.generateValue(), RandomGenerator.generateGroup()));
         }
     }
 
-    public Result process(int T){
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(T, () -> System.out.println("Operações Finalizadas... Formatando o resultado ....\n"));
-        List<IOperation> operations = new ArrayList<>(T);
+    public void process(int T) throws RuntimeException {
+        FinalResult finalResult = new FinalResult();
 
-        startOperations(operations, cyclicBarrier, T);
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(T, () -> {
+            System.out.println("Operações Finalizadas... Formatando o resultado ....\n");
+            finalResult.print();
+        });
 
-        try {
-            cyclicBarrier.await();
-        }catch (Exception ex){
-            System.err.println(ex.getMessage());
-        }
-
-        List<Result> results = operations.stream().map(IOperation::extractProcessing).toList();
-        return this.combineResults(results);
-    }
-
-    private void startOperations(List<IOperation> operations, CyclicBarrier cyclicBarrier, int T) {
-        List<Element> elements = parallelArraySummary.getElements();
+        List<OperationTask> tasks = new ArrayList<>();
 
         int chunkSize = elements.size() / T;
         int extraElements = elements.size() % T;
-        int startIndex = 0;
 
         for (int i = 0; i < T; i++) {
-            int subListSize = chunkSize + (i < extraElements ? 1 : 0);
-            int endIndex = startIndex + subListSize;
+            int start = i * chunkSize;
+            int end = start + (chunkSize - 1) + (i < extraElements ? 1:0);
 
-            IOperation operation = new IOperationImpl(elements.subList(startIndex, endIndex), cyclicBarrier);
-            operations.add(operation);
+            IOperation operation = new IOperationImpl(elements.subList(start, Math.min(end, elements.size())), finalResult);
 
-            new Thread(operation).start();
-            startIndex = endIndex;
+            OperationTask operationTask = new OperationTask(cyclicBarrier, operation);
+            tasks.add(operationTask);
+
+            new Thread(operationTask).start();
         }
-    }
-
-
-    private Result combineResults(List<Result> results){
-        double sumTotal = ResultProcessor.sumListOfTotals(results.stream().map(Result::sumTotal).toList());
-        List<Integer> idsLessThanFive = ResultProcessor.extractIds(results.stream().map(Result::idsLessThanFive).toList());
-        List<Integer> idsGreaterOrEqualToFive = ResultProcessor.extractIds(results.stream().map(Result::idsGreaterOrEqualToFive).toList());
-        Map<Integer, Double> sumTotalsByGroup = ResultProcessor.sumListOfTotalsByGroup(results.stream().map(Result::sumTotalByGroup).toList());
-
-        return new Result(sumTotal, sumTotalsByGroup, idsLessThanFive, idsGreaterOrEqualToFive);
     }
 
 }
