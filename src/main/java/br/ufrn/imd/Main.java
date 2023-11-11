cc0826935fe6fe660f815e8cd13030867448d3f9
pedrolucas27 <plfigueiredo.27@gmail.com>
2023-11-11 15:50:15 -0300

    feat: documentação do código

--- src/main/java/br/ufrn/imd/Main.java
+++ src/main/java/br/ufrn/imd/Main.java
@@ -2,24 +2,31 @@ package br.ufrn.imd;
 
 import br.ufrn.imd.model.Executor;
 
+/**
+ * A classe responsável por iniciar a execução do Executor.
+ */
 public class Main {
 
+    /**
+     * Método principal que inicia o programa.
+     *
+     * @param args Os argumentos da linha de comando, esperados para serem dois inteiros N e T.
+     */
     public static void main(String[] args) {
         int N = 0, T = 0;
 
         try {
             N = Integer.parseInt(args[0]);
             T = Integer.parseInt(args[1]);
-        } catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
             System.err.println("Argument must be an integer.");
             System.exit(1);
         }
 
-        System.out.println("Inciando programa...");
+        System.out.println("Inciando programa...\n");
 
         Executor executor = new Executor(N, T);
         executor.loadElements();
         executor.process();
-
     }
 }
--- src/main/java/br/ufrn/imd/model/FinalResult.java
+++ src/main/java/br/ufrn/imd/model/FinalResult.java
@@ -2,47 +2,76 @@ package br.ufrn.imd.model;
 
 import java.util.*;
 
+/**
+ * A classe FinalResult representa o resultado final acumulado das operações realizadas.
+ */
 public class FinalResult {
 
-    private double sumTotal;
-    private Set<Integer> idsLessThanFive;
-    private Set<Integer> idsGreaterOrEqualToFive;
-    private Map<Integer, Double> sumTotalByGroup;
+    private double sumTotal;                        // Soma total dos valores dos elementos
+    private Set<Integer> idsLessThanFive;           // Conjunto de identificadores cujo valor total é menor que cinco
+    private Set<Integer> idsGreaterOrEqualToFive;   // Conjunto de identificadores cujo valor total é maior ou igual a cinco
+    private Map<Integer, Double> sumTotalByGroup;   // Soma total dos valores dos elementos por grupo
 
-    public FinalResult(){
+
+    /**
+     * Construtor da classe FinalResult. Inicializa as estruturas de dados.
+     */
+    public FinalResult() {
         this.sumTotalByGroup = new HashMap<>();
         this.idsGreaterOrEqualToFive = new HashSet<>();
         this.idsLessThanFive = new HashSet<>();
     }
 
-    public synchronized void updateSumTotal(double sum){
+    /**
+     * Atualiza a soma total dos valores dos elementos.
+     *
+     * @param sum O valor a ser adicionado à soma total.
+     */
+    public synchronized void updateSumTotal(double sum) {
         sumTotal = sumTotal + sum;
     }
 
-    public synchronized void updateSumTotalByGroup(Map<Integer, Double> totalsByGroup){
-        for(Integer group : totalsByGroup.keySet()){
+    /**
+     * Atualiza a soma total dos valores dos elementos por grupo.
+     *
+     * @param totalsByGroup Um mapa representando a soma total por grupo.
+     */
+    public synchronized void updateSumTotalByGroup(Map<Integer, Double> totalsByGroup) {
+        for (Integer group : totalsByGroup.keySet()) {
             double totalMap = Objects.isNull(sumTotalByGroup.get(group)) ? 0 : sumTotalByGroup.get(group);
             sumTotalByGroup.put(group, totalMap + totalsByGroup.get(group));
         }
     }
 
-    public synchronized void updateIdsLessThanFive(List<Integer> ids){
+    /**
+     * Atualiza o conjunto de identificadores cujo total é menor que cinco.
+     *
+     * @param ids Lista de identificadores a serem adicionados ao conjunto.
+     */
+    public synchronized void updateIdsLessThanFive(List<Integer> ids) {
         idsLessThanFive.addAll(ids);
     }
 
-    public synchronized void updateIdsGreaterOrEqualToFive(List<Integer> ids){
+    /**
+     * Atualiza o conjunto de identificadores cujo total é maior ou igual a cinco.
+     *
+     * @param ids Lista de identificadores a serem adicionados ao conjunto.
+     */
+    public synchronized void updateIdsGreaterOrEqualToFive(List<Integer> ids) {
         idsGreaterOrEqualToFive.addAll(ids);
     }
 
-    public void print(){
+    /**
+     * Imprime o resultado final na saída padrão.
+     */
+    public void print() {
         System.out.println("Soma total = " + this.sumTotal);
 
         System.out.println("\nSoma total por grupo:");
         this.sumTotalByGroup.forEach((key, value) -> System.out.println("Grupo " + key + ": " + value));
 
-        System.out.println("\nQtd. de Ids cujo total é menor que 5: "+idsLessThanFive.size()+".");
+        System.out.println("\nQtd. de Ids cujo total é menor que 5: " + idsLessThanFive.size() + ".");
 
-        System.out.println("\nQtd. de Ids cujo total é maior ou igual que 5: "+idsGreaterOrEqualToFive.size()+".\n");
+        System.out.println("\nQtd. de Ids cujo total é maior ou igual a 5: " + idsGreaterOrEqualToFive.size() + ".\n");
     }
-
 }
